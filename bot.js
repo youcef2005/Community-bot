17a5f8a28ee38f954fe84f2a40d3a02b8127def8
youcef2005 <36991100+youcef2005@users.noreply.github.com>
2019-01-11 08:36:48 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,24 @@ client.on('message' , message => {
 }
  });
 
+client.on("message", message => {
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل بنجاح",
+        footer: {
+          text: "Leopard Host"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+});
 
 
 client.login(process.env.BOT_TOKEN);
