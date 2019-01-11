f1952a4cbe36e820a3048f064f409b16b2da5359
youcef2005 <36991100+youcef2005@users.noreply.github.com>
2019-01-11 08:40:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,5 +62,21 @@ client.on("message", message => {
                           }
 });
 
+client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith(prefix + "ping")) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms :signal_strength: ")
+                        .addField('**WebSocket:**',api + " ms :signal_strength: ")
+         message.channel.send({embed:embed});
+                        }
+ });
+
 
 client.login(process.env.BOT_TOKEN);
