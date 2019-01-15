6179d5a1d90f7d69f29295509e3730a1c135be51
youcef2005 <36991100+youcef2005@users.noreply.github.com>
2019-01-15 09:30:05 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,6 +115,33 @@ client.on('message', message => { //bc
     })
     }
     });
+client.on('message',message =>{
+    var prefix = "#";
+    if(message.content.startsWith(prefix + 'top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://media.discordapp.net/attachments/477570106755383307/479229377037598720/22713057_151850495552450_709700562_o.jpg?width=201&height=201")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
 
     
 client.login(process.env.BOT_TOKEN);
