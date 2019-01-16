eafb7c12a9ee2f3fe202b1374d43cbe64925ef9c
youcef2005 <36991100+youcef2005@users.noreply.github.com>
2019-01-16 08:29:05 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.on("message", message => {
 });
 
 client.on('message' , message => {
-  var prefix = "$";
+  var prefix = "#";
   if(message.author.bot) return;
   if(message.content.startsWith(prefix + "ping")) {
  message.channel.send('Pong...').then((msg) => {
